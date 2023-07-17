45516a943584ffdd52efe3c2f66136733d61dbac
YaWenWu1008 <belov3318@gmail.com>
2023-07-17 15:34:05 +0800

    first commit

---
+++ models/products.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, '缺少名稱']
+  },
+  price: {
+    type: Number,
+    required: [true, '缺少價格'],
+    min: [0, '價格太低']
+  },
+  image: {
+    type: String,
+    required: [true, '缺少圖片']
+  },
+  category: {
+    type: String,
+    required: [true, '缺少分類'],
+    enum: {
+      values: ['衣服', '食品', '遊戲', '3C'],
+      message: '分類錯誤'
+    }
+  },
+  sell: {
+    type: Boolean,
+    required: [true, '缺少上架狀態']
+  }
+}, { versionKey: false })
+
+export default mongoose.model('products', schema)
